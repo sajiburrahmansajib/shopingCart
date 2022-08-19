5fe0f9efa4c4e054651f3eabd8e42f0977e7e52a
sajiburrahman <mrahman172152@bscse.uiu.ac.bd>
2022-08-19 12:09:36 +0600

    All function work fine but minus value error not fixed

--- js/case.js
+++ js/case.js
@@ -1,19 +1,27 @@
 document.getElementById('btn-case-plus').addEventListener('click', function () {
     const newValue = makeIncreaseDecress('input-case-value', true);
     makeIncreaseDecressPrice(newValue, 'input-case-value', 59)
+    makeSubToatal()
+
 
 });
 document.getElementById('btn-phone-plus').addEventListener('click', function () {
     const newValue = makeIncreaseDecress('input-phone-value', true);
     makeIncreaseDecressPrice(newValue, 'input-phone-value', 1219)
+    makeSubToatal()
+
 
 });
 document.getElementById('btn-phone-sub').addEventListener('click', function () {
     const newValue = makeIncreaseDecress('input-phone-value', false);
     makeIncreaseDecressPrice(newValue, 'input-phone-value', 1219)
+    makeSubToatal()
+
 });
 document.getElementById('btn-case-sub').addEventListener('click', function () {
     const newValue = makeIncreaseDecress('input-case-value', false);
     makeIncreaseDecressPrice(newValue, 'input-case-value', 59)
+    makeSubToatal()
+
 
 });
--- js/increaseDecrese.js
+++ js/increaseDecrese.js
@@ -19,15 +19,45 @@ function makeIncreaseDecressPrice(newValue, id, price) {
         const totalCost = newValue * price;
         const totalCostElement = document.getElementById('total-cost-case');
         totalCostElement.innerText = totalCost;
+        return totalCost;
     }
     else if (id == 'input-phone-value') {
         const totalCost = newValue * price;
         const totalCostElement = document.getElementById('total-cost-phone');
         totalCostElement.innerText = totalCost;
+        return totalCost;
 
     }
     else {
         return 0;
     }
+
+};
+function setTextElementById(elementId, value) {
+    const subTotalField = document.getElementById(elementId);
+    subTotalField.innerText = value;
+
+};
+
+function makeSubToatal() {
+    const totalCostPhoneField = document.getElementById('total-cost-phone');
+    const totalCostCaseField = document.getElementById('total-cost-case');
+    const subTotalPhoneString = totalCostCaseField.innerText;
+    const subTotalCaseString = totalCostPhoneField.innerText;
+    const subTotalCaseInt = parseInt(subTotalCaseString);
+    const subTotalPhoneInt = parseInt(subTotalPhoneString);
+
+    const subTotal = subTotalCaseInt + subTotalPhoneInt;
+
+    setTextElementById('sub-total', subTotal);
+
+    const taxValue = (subTotal * 0.1).toFixed(2);
+    const taxValueFix = parseInt(taxValue);
+
+    setTextElementById('tax-total', taxValueFix);
+
+    const totalPrice = subTotal + taxValueFix;
+    setTextElementById('total', totalPrice);
+
 
 };
